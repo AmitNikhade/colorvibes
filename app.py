3486fff374801c3f82f3da1d53728f8d6872d747
AmitNikhade <amitnikhadeofficial@gmail.com>
2021-08-06 15:00:35 +0530

    AUg6

--- app.py
+++ app.py
@@ -153,5 +153,5 @@ def upload():
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
     
