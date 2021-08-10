f1c2342940d4b0ac5cc2e24f2feb94e37a262e17
AmitNikhade <amitnikhadeofficial@gmail.com>
2021-08-10 12:26:42 +0530

    r

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ from skimage.color import rgb2gray, gray2rgb, rgb2lab, lab2rgb
 
 # Keras
 
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from keras.models import load_model
+from keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, request, render_template
@@ -38,7 +38,7 @@ def add_header(response):
 MODEL_PATH = 'static/model/Model_final2.h5'
 
 # from keras.applications.nasnet import NASNetLarge
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 inception = MobileNetV2(weights='imagenet', include_top=True)
 
 def inception_embedding(gray_rgb):
